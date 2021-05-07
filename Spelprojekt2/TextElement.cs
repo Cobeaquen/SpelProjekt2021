e13945766e4b825a8b53f929b6277fe0a3b6c2fd
sistabossen69 <oliver_moller02@outlook.com>
2021-05-07 16:37:19 +0200

    UI grejer

--- Spelprojekt2/TextElement.cs
+++ Spelprojekt2/TextElement.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 
 namespace Spelprojekt2
 {
-    class TextElement
+    class TextElement : UIElement
     {
+        protected SpriteFont font;
+        string text;
+        public TextElement(Vector2 position, int width, int height, SpriteFont font, string text) : base(position, width, height)
+        {
+            this.position = position;
+            this.width = width;
+            this.height = height;
+            this.font = font;
+            this.text = text;
+        }
+
+        public override void Draw()
+        {
+            Main.spriteBatch.DrawString(font, text, position, Color.White);
+        }
     }
 }
--- Spelprojekt2/UIElement.cs
+++ Spelprojekt2/UIElement.cs
@@ -14,20 +14,16 @@ namespace Spelprojekt2
         public Vector2 position;
         public int width;
         public int height;
-        public Texture2D texture;
-        public Rectangle bounds;
-        public UIElement(Vector2 position, int width, int height, Texture2D texture)
+        public UIElement(Vector2 position, int width, int height)
         {
             this.position = position;
             this.width = width;
             this.height = height;
-            this.texture = texture;
-            bounds = new Rectangle(new Point((int)position.X, (int)position.Y), new Point(width, height));
         }
 
-        public void Draw()
+        public virtual void Draw()
         {
-            Main.spriteBatch.Draw(texture, position, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+            
         }
     }
 }
