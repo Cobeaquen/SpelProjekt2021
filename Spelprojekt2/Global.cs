b67811687df62f84c656b1851e184e27c2f09ce1
Cobeaquen <37908218+Cobeaquen@users.noreply.github.com>
2021-05-25 19:16:23 +0200

    Add Upgrades

--- Spelprojekt2/Global.cs
+++ Spelprojekt2/Global.cs
@@ -45,6 +45,15 @@ namespace Spelprojekt2
             HP = StartHP;
             Coins = StartCoins;
             GUI.Load();
+            Upgrade[][,] upgrades = new Upgrade[3][,];
+            for (int i = 0; i < upgrades.Length; i++)
+            {
+                upgrades[i] = new Upgrade[2, 3];
+                for (int y = 0; y < upgrades[i].GetLength(1); y++)
+                    for (int x = 0; x < upgrades[i].GetLength(0); x++)
+                        upgrades[i][x, y] = new Upgrade("damage mk1", "Increases damage by 10%", 20);
+            }
+            SaveJSON<Upgrade[][,]>(upgrades, "upgrades.json");
         }
 
         public static void Update(GameTime gameTime)
@@ -61,10 +70,6 @@ namespace Spelprojekt2
                     Effects[i].Update(gameTime);
                 }
                 Main.instance.level.Update(gameTime);
-            }
-            if(HP <= 0)
-            {
-                Paused = true;
             }
             GUI.Update();
             GUI.HandleInput();
